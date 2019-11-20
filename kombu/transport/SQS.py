b25b7be5eb71c03cb81abf59a9c57accafcf58e6
Gal Cohen <galcohen@h-MacBook-Pro-sl-Gal.local>
2019-11-20 05:25:46 +0200

    ..

--- kombu/transport/SQS.py
+++ kombu/transport/SQS.py
@@ -347,6 +347,8 @@ class Channel(virtual.Channel):
     def _schedule_queue(self, queue):
         logger.info('[_schedule_queue] Gal Cohen kombu/kombu/transport/SQS.py')
         if queue in self._active_queues:
+            if self.qos.prefetch_count == 0:
+                self.qos._delivered = OrderedDict()
             if self.qos.can_consume():
                 logger.info('[can_consume] Gal Cohen is true!')
                 self._get_bulk_async(
