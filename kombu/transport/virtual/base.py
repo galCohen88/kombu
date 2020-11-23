c92ceec149ae975852169bd2c1f19c9f2d81edd3
galcohen <gal.cohen@autodesk.com>
2020-11-23 13:46:31 +0200

    ..

--- kombu/transport/virtual/base.py
+++ kombu/transport/virtual/base.py
@@ -239,8 +239,7 @@ class QoS:
 
     def reject(self, delivery_tag, requeue=False):
         """Remove from transactional state and requeue message."""
-        print("GAL virtual base reject")
-        logger.info("GAL virtual base reject")
+        logger.info("GAL virtual base reject~")
         if requeue:
             self.channel._restore_at_beginning(self._delivered[delivery_tag])
         self._quick_ack(delivery_tag)
